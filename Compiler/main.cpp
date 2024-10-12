6631af87a0f5a8038bf193e00347627f7125a66d
elijahcrain <elijah.b.crain@gmail.com>
2024-10-12 17:34:03 -0400

    fix

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -1,3 +1,4 @@
+#include <format>
 #include <iostream>
 #include "Generator.cpp"
 
@@ -7,7 +8,6 @@ main()
     std::string input = "f :: () int {return 2;} g :: () int {var:int=3; return f()+(2*var);}main :: () int { a:int=1;b:int=f();c:int=3; return (a+b)*(f()+g()); }";
     std::unique_ptr<Token> tokens = Lexer::TokensFromInput(input);
     std::unique_ptr<Program> tree = Parser::Parse(tokens);
-    std::cout << std::to_string(static_cast<int>(tokens->kind));
     std::cout<<Generator::GenerateAssembly(tree);
 
     return 0;
